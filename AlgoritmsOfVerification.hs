fd7b3245e7d47263a59d450a8e356bcd05f90265
Mathias Abreu <mathias@Mathias.Ubuntu>
2019-10-17 21:33:26 -0300

    Funcoes de verificação revisados e testados. Funcao de backtracking sendo debbugada ainda. Merge.

--- AlgoritmsOfVerification.hs
+++ AlgoritmsOfVerification.hs
@@ -1,5 +1,5 @@
 module AlgoritmsOfVerification where
-  
+
   -- Metodo verificado! Verifica todas as três condições de inserção de um numero no sudoku.
   -- Retorna 1 se o caractere pode ser inserido.
   -- Retorna 0 se o caractere não pode ser inserido.
@@ -42,21 +42,20 @@ module AlgoritmsOfVerification where
   -- Retorna 1 se o caractere pode ser inserido na linha.
   -- Retorna 0 se o caractere não pode ser inserido na linha.
   selectLinha :: [[Char]] -> Int -> Int -> Char -> Int
-  selectLinha [] linha coluna caractere = 1
   selectLinha (head:tail) linha coluna caractere
-    | (coluna + 1) >= 9                                   = 1
-    | (verificaElementoLinha head 0 linha caractere == 0) = 0
-    | otherwise                                           = selectLinha tail linha (coluna + 1) caractere
+    | (coluna + 1) > 9                                      = 1
+    | ((verificaElementoLinha head 0 linha caractere) == 0) = 0
+    | otherwise                                             = selectLinha tail linha (coluna + 1) caractere
 
   -- Método Privado!
   -- Metodo verificado! Percorre a coluna até encontrar o elemento correspondente da linha e verificar recursivamente se o caractere pode ser inserido ou não.
   -- Retorna 1 se o caractere pode ser inserido na linha.
   -- Retorna 0 se o caractere não pode ser inserido na linha.
   verificaElementoLinha :: [Char] -> Int -> Int -> Char -> Int
-  verificaElementoLinha [] iterator linha caractere = 1
   verificaElementoLinha (head:tail) iterator linha caractere
     | ((iterator == linha) && (head == caractere))  = 0
-    | otherwise                                     = verificaElementoLinha tail (iterator + 1) linha caractere
+    | ((iterator == linha) && (head /= caractere))  = 1
+    | iterator < linha                              = verificaElementoLinha tail (iterator + 1) linha caractere
 
   -- Vincula o setor com as margens corretas e inicia a função que irá verificar o setor.
   selectSetor :: [[Char]] -> Int -> Int -> Char -> Int
@@ -69,21 +68,21 @@ module AlgoritmsOfVerification where
   verificaSetor :: [[Char]] -> Int -> (Int,Int) -> (Int,Int) -> Char -> Int
   verificaSetor [] iterator (linhaInicial,colunaInicial) (linhaFinal,colunaFinal) caractere                 = 1
   verificaSetor (head:tail) iterator (linhaInicial,colunaInicial) (linhaFinal,colunaFinal) caractere
-    | iterator < colunaInicial                                                                              = verificaSetor tail (iterator + 1) (linhaInicial,colunaInicial) (linhaFinal,colunaFinal) caractere
-    | (colunaInicial <= colunaFinal) && (verificaSubcoluna head 0 (linhaInicial,linhaFinal) caractere == 1) = verificaSetor tail iterator (linhaInicial,colunaInicial + 1) (linhaFinal,colunaFinal) caractere
-    | (colunaInicial <= colunaFinal) && (verificaSubcoluna head 0 (linhaInicial,linhaFinal) caractere == 0) = 0
-    | otherwise                                                                                             = 1
+    | iterator < colunaInicial                                                                                                             = verificaSetor tail (iterator + 1) (linhaInicial,colunaInicial) (linhaFinal,colunaFinal) caractere
+    | (iterator == colunaInicial) && (colunaInicial <= colunaFinal) && (verificaSubcoluna head 0 (linhaInicial,linhaFinal) caractere == 1) = verificaSetor tail (iterator + 1) (linhaInicial,colunaInicial + 1) (linhaFinal,colunaFinal) caractere
+    | (iterator == colunaInicial) && (colunaInicial <= colunaFinal) && (verificaSubcoluna head 0 (linhaInicial,linhaFinal) caractere == 0) = 0
+    | (colunaInicial > colunaFinal)                                                                                                        = 1
 
   -- Método Privado!
   -- Função que verifica de fato, uma serie de subcolunas, para saber se tal caractere pode ou não ser inserido ali.
   -- Retorna 1 se o caractere pode ser inserido.
   -- Retorna 0 se o caractere não pode ser inserido.
   verificaSubcoluna :: [Char] -> Int -> (Int,Int) -> Char -> Int
-  verificaSubcoluna [] iterator (inicio, final) caractere = 1
+  verificaSubcoluna matriz 9 (inicio, final) caractere = 1
   verificaSubcoluna (head:tail) iterator (inicio,final) caractere
-    | iterator < inicio                           = verificaSubcoluna tail (iterator + 1) (inicio,final) caractere
-    | inicio > final                              = 1
-    | caractere /= head                           = verificaSubcoluna tail iterator (inicio + 1,final) caractere
+    | iterator < inicio                            = verificaSubcoluna tail (iterator + 1) (inicio,final) caractere
+    | (iterator == inicio) && (inicio > final)     = 1
+    | (iterator == inicio) && (caractere /= head)  = verificaSubcoluna tail (iterator + 1) (inicio + 1,final) caractere
     | otherwise = 0
 
   -- Método Privado!
--- BackTracking.hs
+++ BackTracking.hs
@@ -1,6 +1,30 @@
 
 module BackTracking where
+
   import MethodsAuxiliars
+  import AlgoritmsOfVerification
+
+  testBackTracking :: [[Char]] -> IO()
+  testBackTracking matriz = mostrarSudoku(snd(backTrackingResolution matriz (0,0) 1))
+
+  backTrackingResolution :: [[Char]] -> (Int,Int) -> Int -> (Int,[[Char]])
+  backTrackingResolution matriz (x,y) 10              = (0,matriz)
+  backTrackingResolution matriz (n,9) numero          = (1,matriz)
+  backTrackingResolution matriz (linha,coluna) numero = if ((retornaElemento matriz (0,0) (linha,coluna)) == ' ')
+    then if ((verificaTotal matriz linha coluna (retornaNumerosInChar numero)) == 1)
+      then (backTrackingResolution (adicionaElementoPosicao matriz (0,0) (linha,coluna) (retornaNumerosInChar numero)) (proximaPosicao (linha,coluna)) 1) --matriz (linha,coluna) numero
+      else backTrackingResolution matriz (linha,coluna) (numero + 1) -- matriz (linha,coluna) numero
+    else backTrackingResolution matriz (proximaPosicao (linha,coluna)) 1
+
+  -- Essa função funciona como restart.
+  verificaSudoku :: (Int,[[Char]]) -> [[Char]] -> (Int,Int) -> Int -> (Int,[[Char]])
+  verificaSudoku (1,matriz) matrizAntiga (l,c) numero = (1,matriz)
+  verificaSudoku (0,matriz) matrizAntiga (l,c) numero = backTrackingResolution matrizAntiga (l,c) (numero + 1)
+
+  proximaPosicao :: (Int,Int) -> (Int,Int)
+  proximaPosicao (linha,coluna)
+    | (linha + 1 == 9) = (0,coluna + 1)
+    | otherwise        = (linha + 1,coluna)
 
   -- Metodo para testar a insercao. Não usar!
   testAdiciona :: [[Char]] -> (Int,Int) -> Char -> IO()
@@ -19,3 +43,15 @@ module BackTracking where
   adicionaNaColuna (head:tail) (linhaAtual,linha) caractere
     | linhaAtual < linha = [head] ++ adicionaNaColuna tail (linhaAtual + 1,linha) caractere
     | otherwise          = [caractere] ++ tail
+
+  retornaElemento :: [[Char]] -> (Int,Int) -> (Int,Int) ->  Char
+  retornaElemento [] (linhaAtual,colunaAtual) (linha,coluna) = 'f'
+  retornaElemento (head:tail) (linhaAtual,colunaAtual) (linha,coluna)
+    | colunaAtual < coluna = retornaElemento tail (linhaAtual,colunaAtual + 1) (linha,coluna)
+    | otherwise            = buscaNaColuna head (linhaAtual,linha)
+
+  buscaNaColuna :: [Char] -> (Int,Int) -> Char
+  buscaNaColuna [] (linhaAtual,linhaFinal) = 'f'
+  buscaNaColuna (head:tail) (linhaAtual,linhaFinal)
+    | linhaAtual < linhaFinal = buscaNaColuna tail (linhaAtual + 1,linhaFinal)
+    | otherwise               = head
