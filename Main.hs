6d33e7ace63d5fe422ddf941e26b69791a4034fc
Caio Fernandes <ffmcaio@gmail.com>
2019-10-18 13:36:25 -0300

    òi

--- Main.hs
+++ Main.hs
@@ -3,6 +3,7 @@ module Main where
   import Generator
   import AuxiliaryFunctions
   import AlgoritmsOfVerification
+  import BackTracking
 
   main :: IO()
   main = do
@@ -19,64 +20,52 @@ module Main where
 
   printaMenu :: IO()
   printaMenu = do
-    putStrLn "1. Criar novo jogo"
-    putStrLn "2. Inserir Sudoku Manual"
-    putStrLn "3. Resolvedor de sudoku"
-    putStrLn "4. Sair"
+    putStrLn "1. Gerar Sudoku"
+    putStrLn "2. Resolvedor de sudoku"
+    putStrLn "3. Sair"
     opcao <- getLine
     escolherOpcaoMenu opcao
 
   escolherOpcaoMenu :: String -> IO()
-  escolherOpcaoMenu "1" = do {putStrLn "";
-                              putStrLn "Escolha uma dificuldade:";
-                              putStrLn "1. Facil";
-                              putStrLn "2. Medio";
-                              putStrLn "3. Dificil";
-                              opcao <- getLine;
-                              putStrLn "";
-                              escolherDificuldade opcao
+  escolherOpcaoMenu "1" = do {
+                              gerarSudoku
                               }
-  escolherOpcaoMenu "2" = putStrLn "AQUI CHAMA O METODO QUE INSERE MANUALMENTE"
-  escolherOpcaoMenu "3" = putStrLn "AQUI JA CHAMA O METODO QUE VAI RESOLVENDO O SUDOKU"
-  escolherOpcaoMenu "4" = return ()
+  escolherOpcaoMenu "2" = resolverSudoku
+  escolherOpcaoMenu "3" = return ()
   escolherOpcaoMenu _ = do {putStrLn "";
                             putStrLn "Opçao invalida, tente novamente!";
                             printaMenu
                            }
 
-  escolherDificuldade :: String -> IO()
+  resolverSudoku :: IO()
+  sudokuInserido = [[' ',' ','2',' ',' ','6','8',' ',' '],['3',' ',' ',' ','5',' ',' ',' ','1'],[' ','4',' ','9',' ',' ',' ','3',' '],[' ',' ',' ',' ',' ','4','5',' ','6'],[' ','5',' ',' ','6',' ',' ','1',' '],['1',' ','8','5',' ',' ',' ',' ',' '],[' ','3',' ',' ',' ','7',' ','6',' '],['8',' ',' ',' ','2',' ',' ',' ','3'],[' ',' ','7','4',' ',' ','9',' ',' ']]
+  (a,sudokuResolvido) = backTrackingResolution sudokuInserido (0,0) 1
+  resolverSudoku = do
+    mostrarSudoku sudokuResolvido
+
+  gerarSudoku :: IO()
   sudoku = [[' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' '],[' ',' ',' ',' ',' ',' ',' ',' ',' ']]
-  gerado = gerador sudoku 0 0 100
-  escolherDificuldade "1" = do {mostrarSudoku (gerador sudoku 0 0 300);
-                                escolheCoordenadas
+  gerado = gerador sudoku 0 0 25
+  gerarSudoku = do {
+                                escolheCoordenadas gerado
                                }
-  escolherDificuldade "2" = do {mostrarSudoku (gerador sudoku 0 0 200);
-                                escolheCoordenadas
-                               }
-  escolherDificuldade "3" = do {mostrarSudoku (gerador sudoku 0 0 100);
-                                escolheCoordenadas
-                               }
-  escolherDificuldade _ = do {putStrLn "Opçao invalida, tente novamente!";
-                              printaMenu
-                             }
 
   escolheCoordenadas :: [[Char]] -> IO()
-  escolheCoordenadas matriz = do {putStr "Escolha a linha (0 para desistir):";
+  escolheCoordenadas matriz = do
+    mostrarSudoku matriz
+    putStr "Escolha a linha (9 para desistir):"
+    linha <- readLn :: IO Int
+    putStr "Escolha a coluna (9 para desistir):"
+    coluna <- readLn :: IO Int
+    putStr "Insira o numero: "
+    numero <- readLn :: IO Int
+    let (inteiro,matrizNova) = verificaDesistencia matriz linha coluna (retornaNumerosInChar numero)
+    if (inteiro == 1)
+      then do
+        mostrarSudoku matrizNova
+        putStr "Sudoku concluido!"
+      else escolheCoordenadas matrizNova
 
-              mostrarSudoku matriz
-              linha <- read(getLine);
-              putStr "Escolha a coluna (0 para desistir):";
-              coluna <- read(getLine);
-              putStr "Insira o numero: "
-              numero <- read(getLine);
-              matrizNova <- escolheCoordenadas (verificaDesistencia matriz linha coluna (retornaNumerosInChar numero))
-
-              if (fst(matrizNova) == 1)
-                then putStr "Sudoku concluido!"
-                else escolheCoordenadas snd(matrizNova)
-    }
-
-  verificaDesistencia :: [[Char]] Int -> Int -> (Int,[[Char]])
-  verificaDesistencia matriz "0" _ = putStrLn "aqui chama o resolvedor"
-  verificaDesistencia matriz _ "0" = putStrLn "aqui chama o resolvedor"
-  verificaDesistencia x y = (0,adicionaElementoPosicao matriz (0,0) (x,y))
+  verificaDesistencia :: [[Char]] -> Int -> Int -> Char -> (Int,[[Char]])
+  verificaDesistencia matriz 9 9 z = backTrackingResolution matriz (0,0) 1
+  verificaDesistencia matriz x y z = (0,adicionaElementoPosicao matriz (0,0) (x,y) z)
